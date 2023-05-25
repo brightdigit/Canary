c13777efbdd552a28091456e578acdcaaa0d280d
Leo Dion <leogdion@brightdigit.com>
2023-05-25 11:26:42 -0400

    fixing spm

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,6 +12,7 @@ let package = Package(
     .watchOS(.v3)
   ],
   products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "Canary",
       targets: ["Canary"]
@@ -19,7 +20,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/brightdigit/Prch.git", from: "0.2.1"),
+    .package(url: "https://github.com/brightdigit/Prch.git", from: "1.0.0-alpha.1"),
     .package(name: "PLCrashReporter", url: "https://github.com/microsoft/plcrashreporter.git", from: "1.10.0"),
     .package(name: "Sentry", url: "https://github.com/getsentry/sentry-cocoa.git", from: "7.5.0"),
     .package(url: "https://github.com/shibapm/Komondor", from: "1.1.2"), // dev
@@ -30,6 +31,8 @@ let package = Package(
     .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
   ],
   targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Canary",
       dependencies: ["Prch", "SentryVanilla",
